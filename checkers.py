32fce25f987eb422bb07de947518729deb5fd049
YGeeneY <eyalansky@gmail.com>
2016-03-18 17:36:34 +0200

    Strike half done

--- checkers.py
+++ checkers.py
@@ -1,5 +1,5 @@
 import traceback
-from itertools import repeat
+from itertools import repeat, product
 from string import ascii_letters
 
 
@@ -32,7 +32,7 @@ class Board:
             """
             return {cell: value for cell, value in zip(cell_marker(raw_number),
                                                        repeat(cup_marker(raw_number)))}
-        # list of raws representing initialed deck
+        # list of rows representing initialed deck
         self.state = [produce_raw(raw_number) for raw_number in range(8)]
 
     def action(self, x, y):
@@ -62,49 +62,6 @@ class Board:
         # TODO multi strike
         # TODO multi queen strike
         # TODO state changer
-
-    def move(self, x_d, y_d, x_l, y_l):
-        checker = self.state[x_d][x_l]
-        cell_to_move = self.state[y_d][y_l]
-        if cell_to_move != 'E':
-            return {'success': False, 'cause': 'Move to cell is not empty'}
-
-        self.state[x_d][x_l], self.state[y_d][y_l] = 'E', checker
-        self.change_move()
-        return {'success': True, 'state': self.state}
-
-    def change_move(self):
-        result = ['W', 'B']
-        result.remove(self.next_move)
-        return result[0]
-
-    def recognize_move(self, x_d, y_d, x_l, y_l):
-        checker = self.state[x_d][x_l]
-        if checker != self.next_move:
-            return {'success': False, 'cause': 'It\'s %s turn' % (self.next_move, )}
-
-        if checker in ('B', "W"):
-            diff = abs(x_d - y_d)
-            if diff == 1:
-                to_compare = x_d + 1 if self.next_move == 'B' else x_d - 1
-                if y_d == to_compare:
-                    return {'success': True, 'action': 'move'}
-                else:
-                    return {'success': False, 'cause': 'You can\'t move backwards'}
-
-            elif diff == 2:
-                return {'success': True, 'action': 'strike'}
-
-            else:
-                return {'success': False, 'cause': 'Move is illegal'}
-
-        elif checker in ('BQ', 'WQ'):
-            pass
-            # TODO strike, move for queens
-            # TODO diagonals
-
-        else:
-            return {'success': False, 'cause': 'No checker in current cell'}
 
     @staticmethod
     def separate(x, y):
@@ -143,70 +100,98 @@ class Board:
         else:
             return {'success': False, 'cause': 'Raw identifier is wrong'}
 
-    # def move(self, x, y, queen=False):
-    #     if queen:
-    #         pass
-    #         # TODO queen logic
+    def recognize_move(self, x_d, y_d, x_l, y_l):
+        checker = self.state[x_d][x_l]
+        if checker != self.next_move:
+            return {'success': False, 'cause': 'It\'s %s turn' % (self.next_move, )}
 
-        # else:
-        #     if y_l in self.neighbour(x_l):
-        #         if self.state[x_d][x_l][0] == self.next_move:
-        #             if self.state[y_d][y_l] == 'E':
-        #                 to_compare = x_d + 1 if self.next_move == 'B' else x_d - 1
-        #
-        #                 if y_d == to_compare:
-        #                     return True
+        if checker in ('B', "W"):
+            diff = abs(x_d - y_d)
+            if diff == 1:
+                to_compare = x_d + 1 if self.next_move == 'B' else x_d - 1
+                if y_d == to_compare:
+                    return {'success': True, 'action': 'move'}
+                else:
+                    return {'success': False, 'cause': 'You can\'t move backwards'}
 
-    # def move(self, x, y):
-    #     try:
-    #         x_l = x[0]
-    #         y_l = y[0]
-    #         x_d = int(x[1]) - 1
-    #         y_d = int(y[1]) - 1
-    #     except:
-    #         print(traceback.format_exc())
-    #         return False
-    #
-    #     if self.check(x_l, x_d, y_l, y_d):
-    #         self.state[x_d][x_l] = 'E'
-    #         self.state[y_d][y_l] = self.next_move
-    #         self.switch_user(self.next_move)
-    #         return {'success': True,
-    #                 'board': self.state}
-    #
-    #     return {'success': False}
-    #
-    # def switch_user(self, user):
-    #     if user == 'B':
-    #         self.next_move = 'W'
-    #     else:
-    #         self.next_move = 'B'
-    #
-    # def check(self, x_l, x_d, y_l, y_d):
-    #     if x_d in range(8) and y_d in range(8) and x_l in self.LETTERS and y_l in self.LETTERS:
-    #         if y_l in self.neighbour(x_l):
-    #             if self.state[x_d][x_l][0] == self.next_move:
-    #                 if self.state[y_d][y_l] == 'E':
-    #                     to_compare = x_d + 1 if self.next_move == 'B' else x_d - 1
-    #
-    #                     if y_d == to_compare:
-    #                         return True
-    #     return False
-    #
-    # def neighbour(self, letter):
-    #
-    #     position = self.LETTERS.index(letter)
-    #     if not position or position == 7:
-    #         neighbours = [self.LETTERS[abs(position - 1)], ]
-    #     else:
-    #         neighbours = [self.LETTERS[position - 1], self.LETTERS[position + 1], ]
-    #     return neighbours
+            elif diff == 2:
+                return {'success': True, 'action': 'strike'}
+
+            else:
+                return {'success': False, 'cause': 'Move is illegal'}
+
+        elif checker in ('BQ', 'WQ'):
+            pass
+            # TODO strike, move for queens
+            # TODO diagonals
+
+        else:
+            return {'success': False, 'cause': 'No checker in current cell'}
+
+    def move(self, x_d, y_d, x_l, y_l):
+        checker = self.state[x_d][x_l]
+        cell_to_move = self.state[y_d][y_l]
+        if cell_to_move != 'E':
+            return {'success': False, 'cause': 'Move to cell is not empty'}
+
+        self.state[x_d][x_l], self.state[y_d][y_l] = 'E', checker
+        self.change_next_move()
+        return {'success': True, 'state': self.state}
+
+    @staticmethod
+    def strike_neighbour_cells(x):
+        result = []
+        if x <= 5:
+            result.append(x+2)
+        if x >= 2:
+            result.append(x-2)
+        return result
+
+    def strike_target_cell(self, x_d, x_l, y_d,  y_l):
+        avg = lambda x, y: (x + y) // 2
+
+        striker_column_index = self.LETTERS.index(x_l)
+        target_column_index = self.LETTERS.index(y_l)
+
+        target_column = self.LETTERS[avg(striker_column_index, target_column_index)]
+        target_raw = avg(x_d, y_d)
+        return target_column, target_raw
+
+    def can_strike(self, x_d, x_l, **kwargs):
+        result = []
+
+        striker_column_index = self.LETTERS.index(x_l)
+        columns_index = self.strike_neighbour_cells(striker_column_index)
+        columns = map(lambda x: self.LETTERS[x], columns_index)
+        rows = self.strike_neighbour_cells(x_d)
+
+        strike_cells = product(columns, rows)
+
+        from_cell = self.state[x_d][x_l]
+
+        for i in strike_cells:
+            target = self.strike_target_cell(x_d, x_l, i[1], i[0])
+
+            target_cell = self.state[target[1]][target[0]]
+            strike_cell = self.state[i[1]][i[0]]
+
+            if strike_cell == 'E' and target_cell == self.return_opposite_move():
+                result.append((from_cell, target_cell, strike_cell))
+
+        return result
+
+    def return_opposite_move(self):
+        result = ['W', 'B']
+        result.remove(self.next_move)
+        return result[0]
+
+    def change_next_move(self):
+        self.next_move = self.return_opposite_move()
 
 
 if __name__ == '__main__':
     board = Board()
     from time import time
     start = time()
-    a = board.action('a3', 'b4')
-    print(a, time() - start)
-
+    a = board.can_strike(x_d=2, x_l='c')
+    print(a)
--- server.py
+++ server.py
@@ -5,45 +5,71 @@ import tornado.web
 import tornado.websocket
 from checkers import Board
 import json
-
+from pprint import pprint
 from tornado.options import define, options, parse_command_line
 
 define("port", default=8800, help="run on the given port", type=int)
 
 # we gonna store clients in dictionary..
-clients = dict()
+rooms = {}
 
 
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
-    def get(self):
-        self.render("templates/index.html")
+    def get(self, game_id):
+        self.render("templates/index.html", path=game_id)
 
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
-    board = Board()
+    # def __init__(self, *args, **kwargs):
+    #     self.player = kwargs.pop('player')
+    #     self.player.socket = self
+    #     self.player.left = False
+    #     super(EchoWebSocket, self).__init__(*args, **kwargs)
 
-    def open(self):
-        self.write_message(json.dumps({'board': self.board.state}))
+    def check_origin(self, origin):
+        print(origin)
+        return True
+
+    def open(self, game_id):
+        room = rooms.get(game_id, False)
+        self.game_id = game_id
+
+        if not room:
+            rooms[game_id] = dict(
+                game=Board(),
+                clients=[self]
+            )
+
+        else:
+            rooms[game_id]['clients'].append(self)
+
+        self.write_message(json.dumps({'state': rooms[game_id]['game'].state}))
+
+        pprint(rooms)
+
 
     def on_message(self, message):
         try:
             message = json.loads(message)
-            response = self.board.move(message['from'], message['to'])
-            self.write_message(json.dumps(response))
+            response = rooms[self.game_id]['game'].action(message['from'], message['to'])
+            for client in rooms[self.game_id]['clients']:
+                client.write_message(json.dumps(response))
 
         except ValueError:
             print(traceback.format_exc())
             self.write_message('Bad request')
 
     def on_close(self):
-        print("WebSocket closed")
+        rooms[self.game_id]['clients'].remove(self)
+        pprint(rooms)
 
 
+settings = {'auto_reload': True, 'debug': True}
 app = tornado.web.Application([
-    (r'/', IndexHandler),
-    (r'/websocket', EchoWebSocket),
-])
+    (r'/(?P<game_id>\d\d\d\d\d)', IndexHandler),
+    (r'/websocket/(?P<game_id>\d\d\d\d\d)', EchoWebSocket),
+], **settings)
 
 
 if __name__ == '__main__':
